75c68cffd8c1b5b0ca563a3deb002c4be11775fc
Jairo Buto <jairobuto03@gmail.com>
2025-02-13 17:23:56 +0100

    Model totalmente configuradas, passando para a criacao das controller para tratar das regras de negocios na aplicacao

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,4 +66,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function avaliacoes()
+    {
+        return $this->hasMany(Avaliacao::class);
+    }
 }
