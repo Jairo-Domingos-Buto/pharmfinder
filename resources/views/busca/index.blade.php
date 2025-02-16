c573fd484fc4af887f80006bb0f4c4e7b6e13679
Jairo Buto <jairobuto03@gmail.com>
2025-02-16 23:35:15 +0100

    resolucao do problenma de rotas expiradas, agora passadno para a estilizacao e termino da logica do back end

--- resources/views/busca/index.blade.php
+++ resources/views/busca/index.blade.php
@@ -1,35 +1,4 @@
-{{-- <form action="{{ route('busca.search') }}" method="POST" class="row g-3">
-    @csrf
 
-    <!-- Campo de busca -->
-    <div class="col-md-8">
-        <input type="text" name="nome" class="form-control" placeholder="Digite o nome do medicamento" required>
-    </div>
-
-    <!-- Filtro -->
-    <div class="col-md-2">
-        <select name="filtro" class="form-select">
-            <option value="">Sem Filtro</option>
-            <option value="distancia">Distância</option>
-            <option value="avaliacao">Avaliação</option>
-            <option value="preco">Preço</option>
-        </select>
-    </div>
-
-    <!-- Ordem -->
-    <div class="col-md-1">
-        <select name="ordem" class="form-select">
-            <option value="asc">Crescente</option>
-            <option value="desc">Decrescente</option>
-        </select>
-    </div>
-
-    <!-- Botão de envio -->
-    <div class="col-md-1">
-        <button type="submit" class="btn btn-primary">Buscar</button>
-    </div>
-</form>
---}}
 <!DOCTYPE html>
 <html lang="pt">
 
@@ -60,6 +29,7 @@
     <main class="container">
         <section class="search-box">
             <form class="search-form"  method="POST" action="{{ route('busca.search') }}" id="searchForm">
+                @csrf
                 <input type="text" name="nome" placeholder="Digite o nome do medicamento" required>
 {{--                 <input type="text" placeholder="Digite seu endereço ou CEP" required> --}}
                 <button type="submit">Buscar</button>
